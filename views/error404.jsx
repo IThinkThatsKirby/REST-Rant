a29923706e71ce8e9c7904fed12813b000df5f25
Kirby Ellingson <hkellingson@gmail.com>
2022-03-13 12:07:26 -0500

    Adding and rendering a new View

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,6 +6,12 @@ function error404 () {
         <Def>
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
+                <div>
+                    <img src="https://images.unsplash.com/photo-1470115636492-6d2b56f9146d" alt="woOoOowooOoOoo" />
+                    <div>
+                    Photo by <a href="https://unsplash.com/@heytowner?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">JOHN TOWNER</a> on <a href="https://unsplash.com/s/photos/forest?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                </div>
                 <p>Oops, sorry, we can't find this page!</p>
             </main>
         </Def>
--- views/home.jsx
+++ views/home.jsx
@@ -6,9 +6,18 @@ function home () {
         <Def>
             <main>
                 <h1>Home page</h1>
-                <a href="/places">
-    <button className="btn-primary">Places Page</button>
-</a>
+                <div>
+                    <img src="/images/cheesePizza.jpg" alt="IS CHEESE PIZZA" />
+                    <div>
+                        Photo by <a href="https://unsplash.com/@iavnt?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ivan Torres</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                </div>
+                {/*  redudant since i want a nav bar to be the same across all pages also use bootstrap for this */}
+                <nav id="homeBtn">
+                    <a  href="/places">
+                        <button className="btn-primary">Places Page</button>
+                    </a>
+                </nav>
             </main>
         </Def>
     )
