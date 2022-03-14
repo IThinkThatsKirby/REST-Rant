03a39573b6815af5673d14f961b4bab27d45639a
Kirby Ellingson <hkellingson@gmail.com>
2022-03-14 12:56:24 -0500

    places is updatable

--- controllers/places.js
+++ controllers/places.js
@@ -1,31 +1,26 @@
 const router = require("express").Router();
-
+const places = require("../models/places.js")
 router.get("/", (req, res) => {
-  let places = [
-    {
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisines: "Thai, Pan-Asian",
-      pic: "/images/thaispice.jpg",
-      creditLoc:"https://unsplash.com/@calumlewis?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
-      creditName:"Calum Lewis"
-    },
-    {
-      name: "Coding Cat Cafe",
-      city: "Phoenix",
-      state: "AZ",
-      cuisines: "Coffee, Bakery",
-      pic: "/images/catEating.jpg",
-      creditLoc: "https://unsplash.com/@felicewoelke?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
-      creditName: "Felice Wölke",
-    },
-  ];
   res.render("places/index", { places });
 });
 
 router.get('/new', (req, res) => {
   res.render('places/new')
+})
+
+router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
 
 module.exports = router;
---
+++ models/places.js
@@ -0,0 +1,18 @@
+module.exports = [{
+    name: "H-Thai-ML",
+    city: "Seattle",
+    state: "WA",
+    cuisines: "Thai, Pan-Asian",
+    pic: "/images/thaispice.jpg",
+    creditLoc:"https://unsplash.com/@calumlewis?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+    creditName:"Calum Lewis"
+},
+{
+    name: "Coding Cat Cafe",
+    city: "Phoenix",
+    state: "AZ",
+    cuisines: "Coffee, Bakery",
+    pic: "/images/catEating.jpg",
+    creditLoc: "https://unsplash.com/@felicewoelke?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+    creditName: "Felice Wölke",
+}]
