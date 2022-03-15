7b25d1a60f239fbd89863217766916fe5e586565
Kirby Ellingson <hkellingson@gmail.com>
2022-03-14 19:27:28 -0500

    the idea of a navbar exists

--- views/default.jsx
+++ views/default.jsx
@@ -1,21 +1,44 @@
-const React = require('react')
+const React = require("react");
 
-
-function Def (html) {
-    return (
-        <html>
-            <head>
-                <title>Title</title>
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-                <link rel="stylesheet"  href="/css/style.css" />
-            </head>
-            <body>
-                <div style={{height: 92 +'vh'}}>
-                    {html.children}
-                </div>
-            </body>
-        </html>
-    )
+function Def(html) {
+	return (
+		<html>
+			<head>
+				<title>Title</title>
+				<link
+					href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+					rel="stylesheet"
+					integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+					crossorigin="anonymous"
+				/>
+				<link rel="stylesheet" href="/css/style.css" />
+			</head>
+			<body>
+				<div style={{ height: 92 + "vh" }}>
+					{/* mess with this latter */}
+                        <div className="container">
+                            <div className="row">
+                                <nav className="navbar">
+                                    <ul>
+                                        <li>
+                                            <a className="nav-link" href="/">Home</a>
+                                        </li>
+                                        <li>
+                                            <a className="nav-link" href="/places">Places</a>
+                                        </li>
+                                        <li>
+                                            <a className="nav-link" href="/places/new">Add Place</a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    
+					{html.children}
+				</div>
+			</body>
+		</html>
+	);
 }
 
-module.exports = Def
+module.exports = Def;
