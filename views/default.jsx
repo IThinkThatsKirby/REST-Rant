25b88afcf449f199df0dbc669aac4e0e866543fd
Kirby Douglas Ellingson <hkellingson@gmail.com>
2022-04-18 18:57:45 -0500

    added viewport metastuff

--- views/default.jsx
+++ views/default.jsx
@@ -7,7 +7,8 @@ function Def(html) {
 	return (
 		<html>
 			<head>
-				<title>Title</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+				<title>restrants</title>
 				<script
 					src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
 					integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
